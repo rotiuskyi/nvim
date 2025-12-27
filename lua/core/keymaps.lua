479075a283c31c60d6219d6441237b18afa18640
Roman Otiuskyi <roman.otiuskyi@gmail.com>
2025-12-28 00:35:19 +0200

    feat: add Telescope

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -14,3 +14,16 @@ map("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+map("n", "<leader>ff", function()
+  require("telescope.builtin").find_files()
+end, { desc = "[F]ind [F]iles" })
+map("n", "<leader>fg", function()
+  require("telescope.builtin").live_grep()
+end, { desc = "[F]ind by [G]rep" })
+map("n", "<leader>fb", function()
+  require("telescope.builtin").buffers()
+end, { desc = "[F]ind [B]uffers" })
+map("n", "<leader>fh", function()
+  require("telescope.builtin").help_tags()
+end, { desc = "[F]ind [H]elp" })
+
