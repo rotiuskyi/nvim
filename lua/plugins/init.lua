85f279a056cc0b55043831f5183db73b9bc0a431
Roman Otiuskyi <roman.otiuskyi@gmail.com>
2025-12-28 00:37:46 +0200

    feat: add tree-sitter

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,5 +4,13 @@ require("lazy").setup({
     branch = "0.1.x",
     dependencies = { "nvim-lua/plenary.nvim" },
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter").setup({})
+    end,
+  },
 })
 
