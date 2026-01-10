b8e5545604f2aaaf9f685ca0fde1f8fb48dc8179
Roman Otiuskyi <roman.otiuskyi@gmail.com>
2026-01-11 00:48:10 +0200

    refactor: update file struct

--- init.lua
+++ init.lua
@@ -11,8 +11,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("core.options")
-require("core.keymaps")
-require("core.autocmds")
+require("config")
+require("keymap")
+require("autocmd")
+require("lsp")
 require("plugins")
 
--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -63,4 +63,3 @@ autocmd("LspAttach", {
     end
   end,
 })
-
